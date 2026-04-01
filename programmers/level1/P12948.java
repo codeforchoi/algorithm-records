9e21c939c455e81e19453a526241ce1a8063643c
codeforchoi <codeforchoi@gmail.com>
2026-04-01 17:41:22 +0900

    programmers 12948 핸드폰 번호 가리기

---
+++ programmers/level1/P12948.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class P12948 {
+    public static void main(String[] args) {
+        System.out.println(solution("01033334444"));
+        System.out.println(solution("027778888"));
+    }
+
+    public static String solution(String phone_number) {
+        return phone_number.replaceAll(".(?=.{4})", "*");
+    }
+}
