4c740d518cc4085d29b8d84a16f8058535cb7825
codeforchoi <codeforchoi@gmail.com>
2026-04-25 01:01:02 +0900

    boj 1766 문제집

---
+++ boj/etc2/Q1766.java
@@ -0,0 +1,52 @@
+package etc2;
+
+import java.io.*;
+import java.util.*;
+
+// 위상정렬
+public class Q1766 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new ArrayList[N + 1];
+        int[] inDegree = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            inDegree[b]++;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 오름차순
+
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0) {
+                pq.add(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            int cur = pq.poll();
+            sb.append(cur).append(" ");
+
+            for (int next : graph[cur]) {
+                inDegree[next]--;
+                if (inDegree[next] == 0) {
+                    pq.offer(next);
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
