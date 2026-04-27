79dbfb2e1b8b23711caf2b37f3cc77fe6c715dc1
codeforchoi <codeforchoi@gmail.com>
2026-04-27 23:00:11 +0900

    boj 11404 플로이드

---
+++ boj/etc3/Q11404.java
@@ -0,0 +1,55 @@
+package etc3;
+
+import java.io.*;
+import java.util.*;
+
+//
+public class Q11404 {
+    private static final int INF = 100_000_000; // 적당히 큰 값 (N*가중치 최대값보다 커야 함)
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] dist = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) dist[i][j] = 0;
+                else dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            // 노선이 여러 개일 수 있으므로 최솟값 저장
+            dist[u][v] = Math.min(dist[u][v], w);
+        }
+
+        // 플로이드-워셜 (k: 거쳐가는 노드, i: 시작 노드, j: 끝 노드)
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == INF) sb.append("0 ");
+                else sb.append(dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+}
