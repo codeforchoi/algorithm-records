6b24b51e5073a55e56dad38dea71a3445a773971
codeforchoi <codeforchoi@gmail.com>
2026-08-13 16:13:05 +0900

    programmers 12909 올바른 괄호

---
+++ book/stack/level2/P12909.java
@@ -0,0 +1,35 @@
+package stack.level2;
+
+import java.util.*;
+
+public class P12909 {
+	public static void main(String[] args) {
+		String str1 = "()()";
+		String str2 = "(())()";
+		String str3 = ")()(";
+		String str4 = "(()(";
+		System.out.println(solution(str1));
+		System.out.println(solution(str2));
+		System.out.println(solution(str3));
+		System.out.println(solution(str4));
+	}
+	
+	public static boolean solution(String s) {
+		Deque<Character> stack = new ArrayDeque<>();
+		for(Character ch : s.toCharArray()) {
+			if(ch == '(') {
+				stack.push(ch);
+			} else {
+				if(!stack.isEmpty()) {
+					if(stack.peek() == '(') stack.pop();
+					else return false;
+				} else {
+					return false;
+				}
+			}
+		}
+		
+		if(stack.isEmpty()) return true;
+		return false;	
+    }
+}
