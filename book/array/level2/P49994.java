0aaf709fe154812b26c2641ba3c045b206a6cc7d
codeforchoi <codeforchoi@gmail.com>
2026-08-13 14:43:32 +0900

    programmers 49994 방문 길이

--- book/array/level2/P49994.java
+++ book/array/level2/P49994.java
@@ -1,5 +1,7 @@
 package array.level2;
 
+import java.util.*;
+
 public class P49994 {
 	public static void main(String[] args) {
 		String str1 = "ULURRDLLU";
@@ -9,7 +11,29 @@ public class P49994 {
 	}
 	
 	public static int solution(String dirs) {
-        int answer = 0;
-        return answer;
+		Map<Character, int[]> direction = new HashMap<>();
+		direction.put('U', new int[] {0, 1});
+		direction.put('D', new int[] {0, -1});
+		direction.put('R', new int[] {1, 0});
+		direction.put('L', new int[] {-1, 0});
+		
+		int x = 5;
+		int y = 5;		
+		
+		Set<String> set = new HashSet<>();
+		for (int i = 0; i < dirs.length(); i ++) {
+			int[] dir = direction.get(dirs.charAt(i));
+			int nx = x + dir[0];
+			int ny = y + dir[1];
+			
+			if(nx < 0 || ny < 0 || nx > 10 || ny > 10) continue;
+				
+			set.add(x + " " + y + " " + nx + " " + ny);
+			set.add(nx + " " + ny + " " + x + " " + y);
+			
+			x = nx;
+			y = ny;
+		}
+		return set.size() / 2;
     }
 }
