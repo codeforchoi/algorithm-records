ad06ce2320e83e23af432834fe93296ac2dd8d50
codeforchoi <codeforchoi@gmail.com>
2026-08-19 17:10:52 +0900

    programmers 12945 피보나치 수

---
+++ study/week2/P1_12945.java
@@ -0,0 +1,22 @@
+package week2;
+
+public class P1_12945 {
+	public static void main(String[] args) {
+		System.out.println(solution(3));
+		System.out.println(solution(5));
+	}
+	
+	public static int solution(int n) {
+        int divide = 1234567;
+        int[] fibo = new int[n + 1];
+        
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+        	fibo[i] = (fibo[i - 2] + fibo[i - 1]) % divide;
+        }
+        
+        return fibo[n];
+    }
+}
