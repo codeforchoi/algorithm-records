99f6687887c1282de55ab12c680e1db6d84652e5
codeforchoi <codeforchoi@gmail.com>
2026-08-31 16:01:58 +0900

    book queue_01 요세푸스 문제

---
+++ book/queue/B01.java
@@ -0,0 +1,24 @@
+package queue;
+
+import java.util.*;
+
+public class B01 {
+	public static void main(String[] args) {
+		System.out.println(solution(5, 2));
+	}
+	
+	public static int solution(int n, int k) {
+		Queue<Integer> q = new ArrayDeque<>();
+		for(int i = 1; i <= n; i++) {
+			q.offer(i);
+		}
+		
+		while(q.size() > 1) {
+			for(int i = 0; i < k - 1; i++) {
+				q.offer(q.poll());
+			}
+			q.poll();
+		}
+		return q.poll();
+	}
+}
---
+++ book/stack/level3/P81303.java
@@ -0,0 +1,17 @@
+package stack.level3;
+
+public class P81303 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static String solution(int n, int k, String[] cmd) {
+		
+		
+		
+        String answer = "";
+        return answer;
+    }
+	
+
+}
