c2c0b32db811c0afc9cf8da4db6b7cdb4eeabf76
codeforchoi <codeforchoi@gmail.com>
2026-09-01 13:37:56 +0900

    swea 3499 퍼펙트 셔플 modified

--- swea/d3/S3499.java
+++ swea/d3/S3499.java
@@ -3,6 +3,7 @@ package d3;
 import java.io.*;
 import java.util.*;
 
+// 과제 01
 public class S3499 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
