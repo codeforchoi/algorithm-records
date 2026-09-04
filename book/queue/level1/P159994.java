9473e566ceedcb9093ff318fac84d1c2afd91be4
codeforchoi <codeforchoi@gmail.com>
2026-09-04 17:41:17 +0900

    programmers 159994 카드 뭉치

--- book/queue/level1/P159994.java
+++ book/queue/level1/P159994.java
@@ -1,5 +1,7 @@
 package queue.level1;
 
+import java.util.*;
+
 public class P159994 {
 	public static void main(String[] args) {
 		String[] cards1 = {"i", "drink", "water"};
@@ -14,9 +16,28 @@ public class P159994 {
 	    System.out.println(solution(cards1_2, cards2_2, goal2));
 	}
 
-	public static String solution(String[] cards1, String[] cards2, String[] goal) {
-		String answer = "";
-		return answer;
+	public static String solution(String[] cards1, String[] cards2, String[] goal) {				
+		Queue<String> q1 = new ArrayDeque<>();
+		Queue<String> q2 = new ArrayDeque<>();
+		
+		for(String str : cards1) {
+			q1.offer(str);
+		}
+		
+		for(String str : cards2) {
+			q2.offer(str);
+		}
+		
+		for(String word : goal) {
+			boolean isCards1 = (!q1.isEmpty()) ? q1.peek().equals(word) : false;
+			boolean isCards2 = (!q2.isEmpty()) ? q2.peek().equals(word) : false;
+			
+			if(!isCards1 && !isCards2 ) return "No";
+			
+			if(isCards1) q1.poll();
+			else if (!q2.isEmpty() && isCards2) q2.poll();
+		}
+		return "Yes";
 	}
 
 }
