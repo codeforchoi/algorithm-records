490ee5b6eb4f48e77ca88be688663d08a5a824dd
codeforchoi <codeforchoi@gmail.com>
2026-09-07 09:07:27 +0900

    leetcode 77 combinations

---
+++ leetcode/medium/Lc_46_permutations.java
@@ -0,0 +1,6 @@
+
+public class Lc_46_permutations {
+	public List<List<Integer>> permute(int[] nums) {
+			
+	}
+}
---
+++ leetcode/medium/Lc_77_combinations.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Lc_77_combinations {
+
+	private List<List<Integer>> answer;
+
+	public List<List<Integer>> combine(int n, int k) {
+		answer = new ArrayList<>();	
+		
+		int[] arr = new int[k];
+		
+		dfs(arr, n, k, 0, 1);
+		
+		return answer;
+	}
+
+	private void dfs(int[] arr, int n, int k, int depth, int start) {
+		if (depth == k) {
+			List<Integer> list = new ArrayList<>();
+			for (int num : arr) {
+				list.add(num);
+			}
+			answer.add(list);
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			arr[depth] = i;
+			dfs(arr, n, k, depth + 1, i + 1);
+		}
+	}
+}
