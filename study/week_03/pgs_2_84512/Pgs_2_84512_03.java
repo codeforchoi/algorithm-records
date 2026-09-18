94df9a3e05380fb465a5e2d521e839b214a2f74c
codeforchoi <codeforchoi@gmail.com>
2026-09-18 16:40:33 +0900

    programmers 84512_03 모음 사전

---
+++ study/week_03/pgs_2_84512/Pgs_2_84512_03.java
@@ -0,0 +1,39 @@
+package week_03.pgs_2_84512;
+
+import java.util.*;
+
+public class Pgs_2_84512_03 {
+	public static void main(String[] args) {
+		System.out.println(solution("AAAAE"));
+		System.out.println(solution("AAAE"));
+		System.out.println(solution("I"));
+		System.out.println(solution("EIO"));
+	}
+	/*
+	5 + 5^2 + 5^3 + 5^4 + 5^5 전체경우의 수 
+	
+	1 + 5^1 + 5^2 + 5^3 + 5^4 => 781
+	1 + 5^1 + 5^2 + 5^3 => 156 
+	1 + 5^1 + 5^2 => 31
+	1 + 5^1 => 6
+	1 => 1
+	
+	EEOIU => 1 * 781 + 1 / + 1 * 156 + 1 / 3 * 31 + 1 / 2 * 6 + 1 / + 4 * 1 + 1 
+	*/
+	private static int[] number = {781, 156, 31, 6, 1};
+		
+	public static int solution(String word) {
+		int answer = 0;
+		Map<Character, Integer> map = new HashMap<>();
+		map.put('A', 0);
+		map.put('E', 1);
+		map.put('I', 2);
+		map.put('O', 3);
+		map.put('U', 4);
+		
+		for(int i = 0; i < word.length(); i++) {
+			answer += map.get(word.charAt(i)) * number[i] + 1;
+		}	
+		return answer;
+	}
+}
